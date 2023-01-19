013618e97cca3938056912e7509f2472994ad288
joao <jvacs2019@gmail.com>
2023-01-19 17:12:06 -0300

    menu_publico preparado

--- index.php
+++ index.php
@@ -33,4 +33,10 @@
         </footer>
   </main>
 </body>
+<!-- link para bootstrap -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script src="https://code.jquery.com/jquery-2.2.0.min.js" type="text/javascript"></script>
+
+
 </html>
